45310cd5d954e389d200876769ad69e9c8521d0e
fengyuanz <fengyuantest@frommypc>
2014-08-20 18:16:48 +1000

    master change 4

--- test1/src/Abc.java
+++ test1/src/Abc.java
@@ -5,13 +5,13 @@ public class Abc
 	public static void main(String[] args) 
 	{
 		System.out.println("aha");
+		methodOne();
 	}
 	
 	// Method 1
-	
-	private static void method1()
+	private static void methodOne()
 	{
-		System.out.println("one");
+		System.out.println("one two three four");
 	}
 
 }
