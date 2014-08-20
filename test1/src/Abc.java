6ecb621edfece98a8c39e20f470536318deb264b
fengyuanz <fengyuantest@frommypc>
2014-08-20 17:35:14 +1000

    lala3

--- test1/src/Abc.java
+++ test1/src/Abc.java
@@ -1,8 +1,17 @@
 
-public class Abc {
-  public static void main(String[] args)
-  {
-    System.out.println("aha");
-  }
+public class Abc 
+{
 	// abc
+	public static void main(String[] args) 
+	{
+		System.out.println("aha");
+	}
+	
+	// Method 1
+	
+	private static void method1()
+	{
+		System.out.println("one");
+	}
+
 }
